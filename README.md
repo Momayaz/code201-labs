9006615248534cf77a4bbe0d234d08bc6eff2968
momayaz <momayaz.dr.1@gmail.com>
2020-09-09 16:15:24 +0300

    pair programming

--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 
 This page is to introduce me to the user. 
 
-Contents:
+## Contents:
 * About me game
 * Bio
 * Experience and Education
 * Top ten fun facts about me
+
+## Elements:
+The app.js file was worked on by Momayaz Aldoos and Sally Ammous.
+
+* The driver on this file was Momyaz Aldoos.
+* The navigator on this file was Sally Ammous.
--- js/app.js
+++ js/app.js
@@ -1,28 +1,65 @@
 'use strict';
-
 var userName = prompt('Hello! Please enter your name below:');
 console.log(userName);
-
+var q1, q2, q3, q4, q5, q6, q7;
+var count=0;
 var play = confirm('Welcome to my site ' + userName + ', we are now going to play a game so you get to know me. Okay?');
 
-
-if (play === false) {
-
-    var check = confirm('Are you sure you do not want to play? Click OK to play and Cancel to exit');
-    if (check === true) {
-        game();
-    } else {
-        console.log(userName + ' did not want to play.');
-    }
-
-} else {
+if(!play){
+    alert('Maybe another time!')
+} else{
     var intro = alert('This game is made up of 7 questions: 5 yes/no or y/n and 2 very short answer.');
-    var q1, q2, q3, q4, q5, q6, q7;
-    var x = 0;
+    ques1();
+    ques2();
+    ques3();
+    ques4();
+    ques5();
+    ques6();
+    ques7();
+ 
+    alert('Thank you for playing, your score is '+ Number (count) +' out of 7');
+    alert('Thank you for your time ' + userName + '! This page is about me if you are interested in learning more.');
+}
 
-    q1 = prompt('My biggest fear is snakes.').toLowerCase();
+//function check(){
+    // var check = confirm('Are you sure you do not want to play? Click OK to play and Cancel to exit');
+    // if (check === true) {
+    //     game();
+    // } else {
+    //     console.log(userName + ' did not want to play.');
+    // }
+//}
+
+//function game(){
+    // var intro = alert('This game is made up of 7 questions: 5 yes/no or y/n and 2 very short answer.');
+    // ques1();
+    // ques2();
+    // ques3();
+    // ques4();
+    // ques5();
+    // ques6();
+    // ques7();
+
     
-    while (q1 !== 'yes' && q1 !== 'no' && q1 !== 'y' && q1 !=='n') {
+    // //console.log();
+
+    // //     if (count == 0) {
+    // //     alert('It takes someone who really knows me to get every question wrong ;)');
+    // // } else if (count > 0 && count < 4) {
+    // //     alert('I guess this means we need to get to know each other more. Your score is ' + count + ' out of 7.');
+    // // } else if (count > 3 && count < 7) {
+    // //     alert('You did great! Your score is ' + count + ' out of 7.');
+    // // } else if (count == 7) {
+    // //     alert('Congrats! You got all the questions right.');
+    // // }
+    // alert('Thank you for playing, your score is '+ Number (count) +' out of 7');
+    // alert('Thank you for your time ' + userName + '! This page is about me if you are interested in learning more.');
+//}
+
+function ques1 (){
+    q1 = prompt('My biggest fear is snakes.').toLowerCase();
+
+        while (q1 !== 'yes' && q1 !== 'no' && q1 !== 'y' && q1 !=='n') {
 		q1= prompt('My biggest fear is snakes.').toLowerCase();
     } 
     console.log(q1);
@@ -32,7 +69,7 @@ if (play === false) {
 		case 'yes': case 'y':
 			// console.log('I feel like this was an easy one, but GOOD JOB you are right!');
             alert('I feel like this was an easy one, but GOOD JOB you are right!');
-			console.log(x++);
+		    count++;
 		break;
 
         case 'no': case 'n':
@@ -44,12 +81,11 @@ if (play === false) {
             // console.log('I am very afraid of snakes!');
             alert('I am very afraid of snakes!');
         break;
+    }	
 
-
-	}
-	console.log(x)	
-
-   q2 = prompt('I love being on boats.').toLowerCase();
+}
+function ques2 (){
+    q2 = prompt('I love being on boats.').toLowerCase();
 
     while (q2 !== 'yes' && q2 !== 'no' && q2 !== 'y' && q2 !=='n') {
         q2 = prompt('I love being on boats.').toLowerCase();
@@ -61,12 +97,11 @@ if (play === false) {
 		alert('This is very untrue, I am very afraid of open water.');  
    } else {
         // console.log('You are for sure right!');
-		alert('You are for sure right!');
-        console.log(x++);
+        alert('You are for sure right!');
+        count++;
 	}
-	console.log(x);
-
- 
+}
+function ques3 (){
     q3 = prompt('My favorite color is green.').toLowerCase();
 
     while (q3 !== 'yes' && q3 !== 'no' && q3 !== 'y' && q3 !=='n') {
@@ -79,8 +114,8 @@ if (play === false) {
          case 'yes': case 'y':
             // console.log('You are right I love green, I think it is such an energetic color.');
 			alert('You are right I love green, I think it is such an energetic color.');
-			console.log(x++);
-			break;
+		    count++;
+		break;
 
        case 'no': case 'n':
             // console.log('I mean technically you are not completely wrong because I like all the colors, well, except orange.');
@@ -92,9 +127,8 @@ if (play === false) {
             alert('Colors are great!');
         break;
     }
-	console.log(x);
-
-
+}
+function ques4 (){
     q4 = prompt('I got my first job at 16.').toLowerCase();
 
     while (q4 !== 'yes' && q4 !== 'no' && q4 !== 'y' && q4 !=='n') {
@@ -105,43 +139,43 @@ if (play === false) {
     if (q4 === 'yes' || q4 === 'y') {
         // console.log('You are correct.');
 		alert('You are correct.');
-        console.log(x++);
+        count++;
     } else {
         // console.log('I know this is not very common but it is TRUE.');       
 		alert('I know this is not very common but it is TRUE.');   
    }
-   console.log(x);
+}
+function ques5 (){
+    q5 = prompt('Pizza is my favorite food.').toLowerCase();
 
+    while (q5 !== 'yes' && q5 !== 'no' && q5 !== 'y' && q5 !=='n') {
+     q5  = prompt('True or false: Pizza is my favorite food.').toLowerCase();
+     } 
+     console.log(q5);
+ 
+     if (q5 === 'yes' || q5 === 'y') {
+         // console.log('This was actually a trick question, because how can you just have ONE favorite food?');
+         alert('This was actually a trick question, because how can you just have ONE favorite food?');
+    } else {
+         // console.log('You are right! All food is my favorite food ;)');
+         alert('You are right! All food is my favorite food ;)');
+         count++;
+        }
+ 
 
-   q5 = prompt('Pizza is my favorite food.').toLowerCase();
-
-   while (q5 !== 'yes' && q5 !== 'no' && q5 !== 'y' && q5 !=='n') {
-    q5  = prompt('True or false: Pizza is my favorite food.').toLowerCase();
-    } 
-    console.log(q5);
-
-    if (q5 === 'yes' || q5 === 'y') {
-        // console.log('This was actually a trick question, because how can you just have ONE favorite food?');
-		alert('This was actually a trick question, because how can you just have ONE favorite food?');
-   } else {
-        // console.log('You are right! All food is my favorite food ;)');
-		alert('You are right! All food is my favorite food ;)');
-        console.log(x++);
-    }
-   console.log(x);
-
-
+}
+function ques6 (){
     q6 = prompt('How old do you think I am?');
     console.log(q6);
 
     if (Number(q6) === 24) {
         alert('You got it!');
-        console.log(x++);
+        count++;
     } else {
         for (var i = 3; i > 0; i--) {
             if (Number(q6) === 24) {
                 alert('Yes that is it!')
-                console.log(x++);
+                count++;
                 break;
             } else if (q6 === ' ' || q6 === null || isNaN(Number(q6))){
                 q6 = prompt('Try again, this time try only numbers please. How old do you think I am?');
@@ -157,9 +191,9 @@ if (play === false) {
             }
         }
     }
-    console.log(x);
 
-
+}
+function ques7 (){
     q7 = prompt('What kinds of pets did I have growing up?').toLowerCase();
     while (q7 === ' ' || q7 === null || !isNaN(q7)) {
         q7 = prompt('What kinds of pets did I have growing up?').toLowerCase();
@@ -175,33 +209,16 @@ if (play === false) {
             q7 = prompt('Try again. What pets did I have?');
         } else {
             alert('Yes that is true! I actually had 3 pets: ' + pets.toString() + '.');
-            console.log(x++);
-            break;
+		    count++;
+        break;
         }
         if (j === 1 && pets.indexOf(q7) === -1) {
             //console.log('It is totally fine that you didn\'t guess the right answers, but so you know, I had 3 different kinds of pets growing up: ' + pets.toString() + '.');
             alert('It is totally fine that you didn\'t guess the right answers, but so you know, I had 3 different kinds of pets growing up: ' + pets.toString() + '.');
         }
     }
-
-
-    console.log(x);
-
-    var total = x;
-    console.log(total);
-
-    if (total == 0) {
-        alert('It takes someone who really knows me to get every question wrong ;)');
-    } else if (total > 0 && total < 4) {
-        alert('I guess this means we need to get to know each other more. Your score is ' + total + ' out of 7.');
-    } else if (total > 3 && total < 7) {
-        alert('You did great! Your score is ' + total + ' out of 7.');
-    } else if (total == 7) {
-        alert('Congrats! You got all the questions right.');
-    }
 }
 
-alert('Thank you for your time ' + userName + '! This page is about me if you are interested in learning more.');
 
 
 
